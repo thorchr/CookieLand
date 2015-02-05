0be48f132826d67c83e9627b01237bc527deb31d
Michael A Lundsveen <michael@lundsveen.no>
2015-02-05 11:24:25 +0100

    Initial commit - _s

---
+++ content-single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package cla
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php cla_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'cla' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php cla_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
